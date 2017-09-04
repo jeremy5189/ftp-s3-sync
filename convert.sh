f8cc647665e10229c996c1522f670b2594ddbef4
Jeremy Yen <jeremy5189@gmail.com>
2017-09-04 10:05:05 +0800

    Fixing script

--- convert.sh
+++ convert.sh
@@ -4,13 +4,7 @@
 mkdir mp3/$1
 rm mp3/$1/*.mp3
 
-for wav_file in wav/$1/Completed/*.wav; do
+for wav_file in wav/$1/*.wav; do
     echo "Converting $wav_file"
 	ffmpeg -v 0 -y -i $wav_file -ab 160k "$wav_file.mp3" && mv "$wav_file.mp3" mp3/$1/
 done
-
-# Rename *.wav.mp3 to *.mp3
-rename 's/\.wav.mp3$/\.mp3/' mp3/$1/*.wav.mp3
-
-# Remove 0 and lang code
-rename 's/[A-Z]+0{0,3}//' mp3/$1/*.mp3
---
+++ rename.sh
@@ -0,0 +1,7 @@
+cd mp3/$1
+
+# Rename *.wav.mp3 to *.mp3
+rename 's/\.wav.mp3$/\.mp3/' *.wav.mp3
+
+# Remove 0 and lang code
+rename 's/[A-Z]+0{0,3}//' *.mp3
